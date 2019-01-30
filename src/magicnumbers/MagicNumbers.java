9d141d7569b3e16710dc7dd216f8b1397ee674db
andrzejpolednik <andrzej.polednik@gmail.com>
2019-01-30 22:34:57 +0100

    Test files paths fixed after refactor

--- src/magicnumbers/MagicNumbers.java
+++ src/magicnumbers/MagicNumbers.java
@@ -4,26 +4,25 @@ public class MagicNumbers {
 
     public static void main(String[] args) {
        
-        ExtensionResolver erTxt = new ExtensionResolver("test.txt");
+        ExtensionResolver erTxt = new ExtensionResolver("test_files/test.txt");
         if (erTxt.isExtensionValid()) {
             System.out.println("Extension is valid");
         }
         
-        ExtensionResolver erJpg = new ExtensionResolver("test.jpg");
+        ExtensionResolver erJpg = new ExtensionResolver("test_files/test.jpg");
         if (erJpg.isExtensionValid()) {
             System.out.println("Extension is valid");
         }
-        ExtensionResolver erGif = new ExtensionResolver("test.gif");
+        ExtensionResolver erGif = new ExtensionResolver("test_files/test.gif");
         if (erGif.isExtensionValid()) {
             System.out.println("Extension is valid");
         }
         
-        ExtensionResolver erInvalidGif = new ExtensionResolver("invalid_gif.gif");
+        ExtensionResolver erInvalidGif = new ExtensionResolver("test_files/invalid_gif.gif");
         if (erInvalidGif.isExtensionValid()) {
             System.out.println("Extension is valid");
         }
-        
-       
+
         
     }
     
